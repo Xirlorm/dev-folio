6b375feb1cdb0ed86533b88c9a59adbd2466999a
Xirlorm <xirlorm@gmail.com>
2025-05-05 05:04:28 +0000

    feature: navigation bar indicates active page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Home, MessageSquare, Package, User } from "react-feather";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
   const [activePage, setActivePage] = useState("home");
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/" && location.hash.length) {
+      const el = document.querySelector(location.hash);
+
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
 
   return (
     <nav className="sticky top-0 left-0 flex gap-2 font-extrabold p-1 m-0">
-      <Link to="/" className={activePage == "home" ? "active-page" : ""}>
+      <Link
+        to="/"
+        className={activePage == "home" ? "active-page" : ""}
+        onClick={() => setActivePage("home")}
+      >
         <Home className="nav-icon" />
         &nbsp;Home
       </Link>
       <span>&nbsp;·&nbsp;</span>
-      <Link to="/about" className={activePage == "about" ? "active-page" : ""}>
+      <Link
+        to="/about"
+        className={activePage == "about" ? "active-page" : ""}
+        onClick={() => setActivePage("about")}
+      >
         <User className="nav-icon" />
         &nbsp;About
       </Link>
@@ -20,6 +39,7 @@ function Navbar() {
       <Link
         to="/projects"
         className={activePage == "projects" ? "active-page" : ""}
+        onClick={() => setActivePage("projects")}
       >
         <Package className="nav-icon" />
         &nbsp;Projects
@@ -28,6 +48,9 @@ function Navbar() {
       <a
         href="/#contacts"
         className={activePage == "contact" ? "active-page" : ""}
+        onClick={() => {
+          setActivePage("home");
+        }}
       >
         <MessageSquare className="nav-icon" />
         &nbsp;Contact
