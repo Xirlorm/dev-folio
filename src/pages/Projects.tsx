342e7e371e4cf13cf2cfd4c54847daa6b8972cf0
Xirlorm <xirlorm@gmail.com>
2025-05-07 20:01:31 +0000

    removed unused variable from Projects page  component

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 const Projects: React.FC = () => {
-  const dummyProjects = [];
-
   return (
     <div className="bg-gray-200 rounded-sm py-3 text-3xl my-4 text-center">
       <h3>Coming soon...</h3>
