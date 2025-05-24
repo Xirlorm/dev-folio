aa2fd30e5ec11b911193d4c9bab63733274460ae
Xirlorm <xirlorm@gmail.com>
2025-05-24 04:40:17 +0000

    homepage contacts refactor

--- app/page.tsx
+++ app/page.tsx
@@ -5,28 +5,11 @@ import Link from "next/link";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import Tech from "../components/Tech";
+import { techStacks } from "../lib/techStacks";
+import { contacts } from "../lib/contacts";
 
 export default function Page() {
   const { setActivePage } = useContext(UserContext);
-  const icons = [
-    { icon: "devicon:html5", name: "HTML5" },
-    { icon: "devicon:css3", name: "CSS3" },
-    { icon: "devicon:javascript", name: "JavaScript" },
-    { icon: "devicon:typescript", name: "TypeScript" },
-    { icon: "devicon:react", name: "React" },
-    { icon: "devicon:tailwindcss", name: "TailwindCSS" },
-    { icon: "devicon:sass", name: "Sass" },
-    { icon: "devicon:git", name: "Git" },
-    { icon: "devicon:github", name: "GitHub" },
-    { icon: "devicon:vitejs", name: "ViteJS" },
-    { icon: "devicon:webpack", name: "Webpack" },
-    { icon: "devicon:vscode", name: "VSCode" },
-    { icon: "devicon:neovim", name: "Neovim" },
-    { icon: "devicon:linux", name: "Linux" },
-    { icon: "devicon:linuxmint", name: "Linux Mint" },
-    { icon: "vscode-icons:file-type-jest", name: "Jest" },
-    { icon: "vscode-icons:file-type-vitest", name: "Vitest" },
-  ];
 
   return (
     <main className="home">
@@ -53,7 +36,7 @@ export default function Page() {
       <section>
         <h2>My Tech Stack</h2>
         <ul className="tech-stack">
-          {icons.map((icon) => (
+          {techStacks.map((icon) => (
             <Tech icon={icon} key={icon.name} />
           ))}
         </ul>
@@ -66,61 +49,27 @@ export default function Page() {
         </Link>
       </section>
       <section className="featured-projects">
-        <h2>Featured Projects</h2>
+        <h2>Featured projects</h2>
         <div className="bg-gray-200 text-center rounded-md p-1.5">
           Coming soon...
         </div>
       </section>
       <section id="contacts" className="contacts">
-        <h2>How do you reach me?</h2>
+        <h2>Connect with me</h2>
         <div>
-          <ul>
-            {/* remove and place into own component, use loop to create them before showing them here.
-          Do same for tech stacks too */}
-            <li>
-              <Link href="https://www.reddit.com/user/xirlohm">
-                <Icon
-                  className="contact-icon"
-                  icon="simple-icons:reddit"
-                  height={28}
-                  width={28}
-                />
-                <span>Reddit</span>
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.linkedin.com/in/xirlorm">
-                <Icon
-                  className="contact-icon"
-                  icon="simple-icons:linkedin"
-                  height={28}
-                  width={28}
-                />
-                <span>LinkedIn</span>
-              </Link>
-            </li>
-            <li>
-              <Link href="https://x.com/Xirlorm">
-                <Icon
-                  className="contact-icon"
-                  icon="simple-icons:x"
-                  height={28}
-                  width={28}
-                />
-                <span>X</span>
-              </Link>
-            </li>
-            <li>
-              <Link href="mailto:https://xirlorm@gmail.com">
-                <Icon
-                  className="contact-icon"
-                  icon="simple-icons:gmail"
-                  height={28}
-                  width={28}
-                />
-                <span>Email</span>
-              </Link>
-            </li>
+          <ul className="my-5">
+            {contacts.map((contact) => (
+              <li key={contact.name}>
+                <Link href={contact.link}>
+                  <Icon
+                    className="contact-icon"
+                    icon={contact.icon}
+                    width={28}
+                  />
+                  <span>{contact.name}</span>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </section>
---
+++ lib/contacts.tsx
@@ -0,0 +1,22 @@
+export const contacts = [
+  {
+    name: "Reddit",
+    icon: "simple-icons:reddit",
+    link: "https://www.reddit.com/user/xirlohm",
+  },
+  {
+    name: "LinkedIn",
+    icon: "simple-icons:linkedin",
+    link: "https://www.linkedin.com/in/xirlorm",
+  },
+  {
+    name: "X",
+    icon: "simple-icons:x",
+    link: "https://x.com/Xirlorm",
+  },
+  {
+    name: "Gmail",
+    icon: "simple-icons:gmail",
+    link: "mailto:https://xirlorm@gmail.com",
+  },
+];
