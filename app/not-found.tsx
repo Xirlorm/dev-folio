276af75d911bd9edd96c6359c3a26fecedceb31f
Xirlorm <xirlorm@gmail.com>
2025-06-16 12:14:38 +0000

    updated 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import Note from "../components/ui/Note";
+
 export default function NotFound() {
-    return <>Page not found!</>
+  return (
+    <div className="flex flex-col items-center justify-center min-h-100 h-full text-center p-4">
+      <h1 className="text-4xl font-bold mb-3">404 - Page Not Found</h1>
+      <div className="max-w-lg">
+        The page you are looking for does not exist. Please check the URL or
+        return to the homepage.
+      </div>
+      <Link href="/">Go to Homepage</Link>
+      <br />
+      <Note>
+        If you believe this is an error, please contact support.
+        <br />
+        <a href="mailto:https://xirlorm@gooogle.com">Contact Support</a>
+      </Note>
+      <br />
+    </div>
+  );
 }
